4fb2e2394a081abf92e8e044170ed2b6b9e311c3
lucaspires31 <lucas.alvespires31@gmail.com>
2026-04-01 20:57:14 -0300

    feat: create ClienteRepository

---
+++ backend-totem/backend-totem/src/main/java/com/totem/backendtotem/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.totem.backendtotem.repository;
+
+import com.totem.backendtotem.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByCpf(String cpf);
+}
